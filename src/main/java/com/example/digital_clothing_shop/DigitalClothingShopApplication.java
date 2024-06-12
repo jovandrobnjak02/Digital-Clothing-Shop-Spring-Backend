753e5bc6c312878f4ed53c40a8d87a18c5370a1b
jovandrobnjak02 <drobnjakj02@gmail.com>
2024-06-12 11:15:04 +0200

    Initialize project

---
+++ src/main/java/com/example/digital_clothing_shop/DigitalClothingShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.digital_clothing_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalClothingShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalClothingShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/digital_clothing_shop/DigitalClothingShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digital_clothing_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalClothingShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
