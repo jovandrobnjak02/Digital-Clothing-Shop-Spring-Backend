272231e13d8a31bd37d201f274332901ea7705cd
jovandrobnjak02 <drobnjakj02@gmail.com>
2024-06-14 10:49:59 +0200

    Minor fixes

--- src/main/java/com/example/digital_clothing_shop/Controllers/ClothingController.java
+++ src/main/java/com/example/digital_clothing_shop/Controllers/ClothingController.java
@@ -49,6 +49,7 @@ public class ClothingController {
 
     @DeleteMapping("/review/delete")
     public ResponseEntity<Void> deleteReview(@PathVariable int reviewId){
+        
         this.clothingReviewService.deleteReview(reviewId);
 
         return ResponseEntity.ok().<Void>build();
